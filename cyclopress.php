5364e4a62da576bdcc17116353bb8ab5c6a9cdf1
anukit <anukit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-08 20:51:51 +0000

    Adding tag 1.3.4.
    git-svn-id: http://plugins.svn.wordpress.org/cyclopress/trunk@78336 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cyclopress.php
+++ cyclopress.php
@@ -26,7 +26,7 @@ Author URI: http://amwhalen.com
 */
 
 
-$cy_version = '1.3.3';
+$cy_version = '1.3.4';
 $cy_db_version = '1.0';
 $cy_graph_dir = 'graphs';
 $cy_graph_dir_full = dirname(__FILE__).'/'.$cy_graph_dir;
