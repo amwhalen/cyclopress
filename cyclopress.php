2734fc88fee0aeffdbbb53ec902a6faf5d2105ff
anukit <anukit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-22 21:08:07 +0000

    Adding bikes and types tables.
    git-svn-id: http://plugins.svn.wordpress.org/cyclopress/trunk@81869 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cyclopress.php
+++ cyclopress.php
@@ -27,7 +27,7 @@ Author URI: http://amwhalen.com
 
 
 $cy_version = '1.3.5';
-$cy_db_version = '1.3';
+$cy_db_version = '1.4';
 $cy_graph_dir = 'graphs';
 $cy_graph_dir_full = dirname(__FILE__).'/'.$cy_graph_dir;
 $cy_dir = get_bloginfo('url').'/wp-content/plugins/'.basename(dirname(__FILE__));
