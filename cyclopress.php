6e788e13145bbee4fceb43faa9a9a45ec15b969e
anukit <anukit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-01 04:21:55 +0000

    Adding the ability to add and manage bikes.
    git-svn-id: http://plugins.svn.wordpress.org/cyclopress/trunk@90920 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cyclopress.php
+++ cyclopress.php
@@ -1376,7 +1376,7 @@ function cy_manage_bikes_page() {
 						<td>No bikes! What are you riding these days?</td>
 					</tr>
 					<tr>
-						<th><a href="?page=cyclopress/cyclopress.php&manage_bikes=1">Add a new bike.</a></th>
+						<td><a href="?page=cyclopress/cyclopress.php&add_bikes=1">Add a new bike.</a></td>
 					</tr>
 					</tbody>
 					<?php
